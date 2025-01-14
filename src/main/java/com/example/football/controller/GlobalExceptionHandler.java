29c7022b155e4c81d470ee03ed00e48f024ca951
Jinwoo <norang2810@naver.com>
2025-01-14 17:36:49 +0900

    commit

---
+++ src/main/java/com/example/football/controller/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.example.football.controller;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public String handleMaxSizeException(MaxUploadSizeExceededException exc, RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute("message", "파일 크기가 제한을 초과했습니다. 10MB 이하의 파일을 업로드하세요.");
+        return "redirect:/community";
+    }
+}
